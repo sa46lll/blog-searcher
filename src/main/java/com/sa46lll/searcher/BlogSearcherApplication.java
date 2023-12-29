80218801e147a9aca383aece1a4273f9138e9de4
sa46lll <azure394@gmail.com>
2023-12-29 17:54:15 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/sa46lll/searcher/BlogSearcherApplication.java
@@ -0,0 +1,13 @@
+package com.sa46lll.searcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSearcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogSearcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sa46lll/searcher/BlogSearcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sa46lll.searcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSearcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
